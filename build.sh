13c16db62696421345c3ea5d861e085b3c96cf86
Eduardo Pérez Regín <30558331+EdPeReg@users.noreply.github.com>
2023-04-01 14:48:21 -0600

    If statement fixed

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "Compiling software"
 gcc -Wall -Wextra -fsanitize=address -pedantic-errors main.c -o main
 date
 
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo "Compiling successfully"
 else
   echo "Error compiling" >&2
